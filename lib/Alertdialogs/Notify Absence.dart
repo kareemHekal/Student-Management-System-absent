c06556ea13569fc48765f86fe455deb65d848b9f
East-Sound <kareemhekal19@gmail.com>
2025-10-05 22:45:55 +0300

    feat: Add option to notify father of absence and enhance messaging
    This commit introduces several new features and improvements related to notifying parents and students of absences.
    
    - **Notify Father Option:**
        - A "Father" option has been added to the absence notification dialog, allowing users to send a WhatsApp message specifically to the student's father.
        - The UI in `StudentWidget.dart` now displays "Father Number" and "Mother Number" as separate fields.
    
    - **Enhanced WhatsApp Messages:**
        - The content of the absence notification messages has been customized for the father, mother, and student, providing more specific and formal wording.
        - A `constants.dart` file has been created to store the teacher's name, ensuring it can be easily updated and consistently used across all messages.
    
    - **Code Refinements:**
        - The logic in `StudentWidget.dart` has been updated to handle sending messages to the father.
        - Debug print statements in `Notify Absence.dart` have been corrected for clarity.

--- lib/Alertdialogs/Notify Absence.dart
+++ lib/Alertdialogs/Notify Absence.dart
@@ -5,9 +5,10 @@ import '../colors_app.dart';
 class SelectRecipientDialogContent extends StatelessWidget {
   final VoidCallback sendMessageToMother;
   final VoidCallback sendMessageToStudent;
-
+  final VoidCallback sendMessageToFather;
   const SelectRecipientDialogContent({
     Key? key,
+    required this.sendMessageToFather,
     required this.sendMessageToMother,
     required this.sendMessageToStudent,
   }) : super(key: key);
@@ -17,13 +18,20 @@ class SelectRecipientDialogContent extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-
+        ListTile(
+          title: Text('Father', style: TextStyle(color: app_colors.orange),),
+          onTap: () {
+            Navigator.of(context).pop();
+            sendMessageToFather();
+            print("father");
+          },
+        ),
         ListTile(
           title: Text('Mother',style: TextStyle(color: app_colors.orange),),
           onTap: () {
             Navigator.of(context).pop();
             sendMessageToMother();
-            print("mothererer");
+            print("mother");
 
           },
         ),
