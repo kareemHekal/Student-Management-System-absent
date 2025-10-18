d76b1838ae78c41b221bb5326b0fb26d94bbb9fc
Kareem Hekal <kareemhekal19@gmail.com>
2025-10-18 23:19:29 +0300

    make the arabic dirction for the text

--- lib/bottomShets/more Bottom Sheet In Absent Page.dart
+++ lib/bottomShets/more Bottom Sheet In Absent Page.dart
@@ -29,27 +29,29 @@ class CustomBottomSheet extends StatefulWidget {
 }
 
 class _CustomBottomSheetState extends State<CustomBottomSheet> {
-//functions================================================================================================================================
   String _buildNotesForDate(Studentmodel student, String dateKey) {
     if (student.notes == null || student.notes!.isEmpty) {
       return ("There are no notes");
     }
-
-    // Find note for the selected date
     String? noteForSelectedDate;
     for (var note in student.notes!) {
       if (note.containsKey(dateKey)) {
         noteForSelectedDate = note[dateKey];
-        break; // Stop once we find the note for the selected date
+        break;
       }
     }
-
-    // Display the note for the selected date
     if (noteForSelectedDate != null) {
-      return (" $noteForSelectedDate");
+      return ("$noteForSelectedDate");
     } else {
       return ("No notes for $dateKey");
     }
+  }
+
+  pw.TextDirection _getTextDirection(String text) {
+    final arabicRegex = RegExp(r'[\u0600-\u06FF]');
+    return arabicRegex.hasMatch(text)
+        ? pw.TextDirection.rtl
+        : pw.TextDirection.ltr;
   }
 
   _generatePdf(BuildContext context) async {
@@ -57,7 +59,6 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
     final fontData = await rootBundle.load("fonts/NotoKufiArabic-Regular.ttf");
     final pw.Font font = pw.Font.ttf(fontData);
 
-    // Access information from the widget and the filtered list
     final String selectedDate = widget.absenceModel.date;
     final String day = widget.selectedDay;
     final String grade = widget.magmo3aModel.grade ?? 'Unknown Grade';
@@ -65,7 +66,6 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
         widget.magmo3aModel.time?.format(context) ?? 'Unknown Time';
     final int absentCount = widget.filteredStudentsList.length;
 
-    // Create a list of students to display in pairs
     List<List<Studentmodel>> studentChunks = [];
     for (var i = 0; i < absentCount; i += 2) {
       studentChunks.add(
@@ -74,57 +74,70 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
       );
     }
 
-    // Create the first page with the attendance report
     pdf.addPage(
       pw.Page(
         pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
-          return pw.Stack(
-            children: [
-              // Content over the background
-              pw.Column(
-                crossAxisAlignment: pw.CrossAxisAlignment.center,
-                children: [
-                  // Header information
-                  pw.Text("Attendance Report",
-                      style: pw.TextStyle(font: font, fontSize: 16)),
-                  pw.SizedBox(height: 10),
-                  pw.Text("Date: $selectedDate",
-                      style: pw.TextStyle(font: font, fontSize: 12)),
-                  pw.Text("Day: $day",
-                      style: pw.TextStyle(font: font, fontSize: 12)),
-                  pw.Text("Grade: $grade",
-                      style: pw.TextStyle(font: font, fontSize: 12)),
-                  pw.Text("Time: $time",
-                      style: pw.TextStyle(font: font, fontSize: 12)),
-                  pw.Text("Total Absent Students: $absentCount",
-                      style: pw.TextStyle(font: font, fontSize: 12)),
-                  pw.Divider(),
-
-                  // Show first 2 students on the first page
-                  if (studentChunks.isNotEmpty)
-                    pw.Row(
-                      mainAxisAlignment: pw.MainAxisAlignment.center,
-                      children: [
+          return pw.Directionality(
+            textDirection: pw.TextDirection.rtl,
+            child: pw.Column(
+              crossAxisAlignment: pw.CrossAxisAlignment.center,
+              children: [
+                pw.Text(
+                  "Attendance Report",
+                  style: pw.TextStyle(font: font, fontSize: 16),
+                  textDirection: _getTextDirection("Attendance Report"),
+                ),
+                pw.SizedBox(height: 10),
+                pw.Text(
+                  "Date: $selectedDate",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection: _getTextDirection("Date: $selectedDate"),
+                ),
+                pw.Text(
+                  "Day: $day",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection: _getTextDirection(day),
+                ),
+                pw.Text(
+                  "Grade: $grade",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection: _getTextDirection(grade),
+                ),
+                pw.Text(
+                  "Time: $time",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection: _getTextDirection(time),
+                ),
+                pw.Text(
+                  "Total Absent Students: $absentCount",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection:
+                  _getTextDirection("Total Absent Students: $absentCount"),
+                ),
+                pw.Divider(),
+                if (studentChunks.isNotEmpty)
+                  pw.Row(
+                    mainAxisAlignment: pw.MainAxisAlignment.center,
+                    children: [
+                      pw.Expanded(
+                        child: _buildStudentCard(studentChunks[0][0], font),
+                      ),
+                      if (studentChunks[0].length > 1)
                         pw.Expanded(
-                          child: _buildStudentCard(studentChunks[0][0], font),
+                          child: _buildStudentCard(studentChunks[0][1], font),
                         ),
-                        if (studentChunks[0].length > 1)
-                          pw.Expanded(
-                            child: _buildStudentCard(studentChunks[0][1], font),
-                          ),
-                      ],
-                    ),
-                  pw.SizedBox(height: 20), // Space after the first row
-                ],
-              ),
-            ],
+                    ],
+                  ),
+                pw.SizedBox(height: 20),
+              ],
+            ),
           );
         },
       ),
     );
 
-    // Create additional pages for remaining students
+
     for (var i = 1; i < studentChunks.length; i++) {
       pdf.addPage(
         pw.Page(
@@ -146,7 +159,7 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
                       ),
                   ],
                 ),
-                pw.SizedBox(height: 20), // Space after the row
+                pw.SizedBox(height: 20),
               ],
             );
           },
@@ -154,7 +167,6 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
       );
     }
 
-    // Print the document
     await Printing.layoutPdf(
       onLayout: (PdfPageFormat format) async => pdf.save(),
     );
@@ -163,77 +175,60 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
   pw.Widget _buildStudentCard(Studentmodel student, pw.Font font) {
     String note = _buildNotesForDate(student, widget.absenceModel.date);
     return pw.Container(
-      margin: const pw.EdgeInsets.all(8.0), // Space around each card
-      padding: const pw.EdgeInsets.all(16.0), // Internal padding of the card
+      margin: const pw.EdgeInsets.all(8.0),
+      padding: const pw.EdgeInsets.all(16.0),
       decoration: pw.BoxDecoration(
         borderRadius: pw.BorderRadius.circular(10),
-        color: PdfColors.white, // Background color of the card set to white
-        border: pw.Border.all(
-            color: PdfColors.black, width: 2), // Set border color to black
+        color: PdfColors.white,
+        border: pw.Border.all(color: PdfColors.black, width: 2),
       ),
       child: pw.Column(
         crossAxisAlignment: pw.CrossAxisAlignment.start,
         children: [
-          // Name with split label and value
           pw.Row(
             mainAxisAlignment: pw.MainAxisAlignment.start,
             children: [
-              pw.Text(
-                "Name: ",
-                style: pw.TextStyle(font: font, fontSize: 12),
-              ),
-              pw.Text(
-                student.name ?? 'Unnamed Student',
-                style: pw.TextStyle(font: font, fontSize: 12),
-                textDirection: pw
-                    .TextDirection.rtl, // Set text direction to RTL for Arabic
-              ),
+              pw.Text("Name: ",
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection: _getTextDirection("Name: ")),
+              pw.Text(student.name ?? 'Unnamed Student',
+                  style: pw.TextStyle(font: font, fontSize: 12),
+                  textDirection:
+                  _getTextDirection(student.name ?? 'Unnamed Student')),
             ],
           ),
-          pw.SizedBox(height: 8), // Space between name and next field
-
-          // Phone Number
-          pw.Text(
-            "Phone Number: ${student.phoneNumber ?? 'N/A'}",
-            style: pw.TextStyle(font: font, fontSize: 12),
-          ),
-          pw.SizedBox(height: 8), // Space between phone number and next field
-
-          // Mother Number
-          pw.Text(
-            "Mother Number: ${student.motherPhone ?? 'N/A'}",
-            style: pw.TextStyle(font: font, fontSize: 12),
-          ),
-          pw.SizedBox(height: 8), // Space between mother number and next field
-
-          // Father Number
-          pw.Text(
-            "Father Number: ${student.fatherPhone ?? 'N/A'}",
-            style: pw.TextStyle(font: font, fontSize: 12),
-          ),
-
-          // Grade
-          pw.Text(
-            "Grade: ${student.grade ?? 'N/A'}",
-            style: pw.TextStyle(font: font, fontSize: 12),
-          ),
-          pw.SizedBox(height: 8), // Space between father number and next field
-          pw.Text(
-            textDirection: pw.TextDirection.rtl,
-            note,
-            style: pw.TextStyle(font: font, fontSize: 12),
-          ),
+          pw.SizedBox(height: 8),
+          pw.Text("Phone Number: ${student.phoneNumber ?? 'N/A'}",
+              style: pw.TextStyle(font: font, fontSize: 12),
+              textDirection:
+              _getTextDirection(student.phoneNumber ?? 'N/A')),
+          pw.SizedBox(height: 8),
+          pw.Text("Mother Number: ${student.motherPhone ?? 'N/A'}",
+              style: pw.TextStyle(font: font, fontSize: 12),
+              textDirection:
+              _getTextDirection(student.motherPhone ?? 'N/A')),
+          pw.SizedBox(height: 8),
+          pw.Text("Father Number: ${student.fatherPhone ?? 'N/A'}",
+              style: pw.TextStyle(font: font, fontSize: 12),
+              textDirection:
+              _getTextDirection(student.fatherPhone ?? 'N/A')),
+          pw.Text("Grade: ${student.grade ?? 'N/A'}",
+              style: pw.TextStyle(font: font, fontSize: 12),
+              textDirection:
+              _getTextDirection(student.grade ?? 'N/A')),
+          pw.SizedBox(height: 8),
+          pw.Text(note,
+              style: pw.TextStyle(font: font, fontSize: 12),
+              textDirection: _getTextDirection(note)),
         ],
       ),
     );
   }
 
-  //functions================================================================================================================================
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150, // ارتفاع الـ Bottom Sheet
+      height: 150,
       decoration: BoxDecoration(
         color: app_colors.ligthGreen,
         borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
@@ -248,50 +243,35 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
               thickness: 5,
               color: app_colors.orange,
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 _buildIconButton(
-                  imagePath: "assets/icon/printer.png", // مسار صورة الطباعة
+                  imagePath: "assets/icon/printer.png",
                   label: "Print",
                   onPressed: () async {
                     if (widget.filteredStudentsList.isNotEmpty) {
                       await _generatePdf(context);
                     } else {
-                      // Show AlertDialog when there are no absent students
                       showDialog(
                         context: context,
                         builder: (BuildContext context) {
                           return AlertDialog(
-                            title: Text(
-                              "No Absent Students",
-                              style: TextStyle(
-                                  color: app_colors.orange), // Orange title
-                            ),
-                            content: Text(
-                              "There are no absent students to export.",
-                              style: TextStyle(
-                                  color: app_colors
-                                      .ligthGreen), // Light green message
-                            ),
+                            title: Text("No Absent Students",
+                                style: TextStyle(color: app_colors.orange)),
+                            content: Text("There are no absent students to export.",
+                                style: TextStyle(color: app_colors.ligthGreen)),
                             actions: <Widget>[
                               TextButton(
-                                child: Text(
-                                  'OK',
-                                  style: TextStyle(
-                                      color: app_colors
-                                          .orange), // Orange button text
-                                ),
+                                child: Text('OK',
+                                    style: TextStyle(color: app_colors.orange)),
                                 onPressed: () {
                                   Navigator.of(context).pop();
                                 },
                               ),
                             ],
-                            backgroundColor: app_colors
-                                .ligthGreen, // Light green background for the dialog
+                            backgroundColor: app_colors.ligthGreen,
                           );
                         },
                       );
@@ -299,22 +279,26 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
                   },
                 ),
                 _buildIconButton(
-                  imagePath: "assets/icon/done.png", // مسار صورة الإنهاء
+                  imagePath: "assets/icon/done.png",
                   label: "Students attending",
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => StudentsAttending(
-                                absenceModel: AbsenceModel(
-                                    date: widget.absenceModel.date,
-                                    numberOfStudents: widget.absenceModel.numberOfStudents,
-                                    absentStudents: widget.absenceModel.absentStudents,
-                                    attendStudents: widget.absenceModel.attendStudents),
-                                magmo3aModel: widget.magmo3aModel,
-                                selectedDay: widget.selectedDay,
-                              )),
-                      // Removes all previous routes
+                        builder: (context) => StudentsAttending(
+                          absenceModel: AbsenceModel(
+                            date: widget.absenceModel.date,
+                            numberOfStudents:
+                            widget.absenceModel.numberOfStudents,
+                            absentStudents:
+                            widget.absenceModel.absentStudents,
+                            attendStudents:
+                            widget.absenceModel.attendStudents,
+                          ),
+                          magmo3aModel: widget.magmo3aModel,
+                          selectedDay: widget.selectedDay,
+                        ),
+                      ),
                     );
                   },
                 ),
@@ -326,10 +310,8 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
                       context: context,
                       builder: (context) => AlertDialog(
                         backgroundColor: app_colors.ligthGreen,
-                        title: const Text(
-                          "Delete Absence",
-                          style: TextStyle(color: app_colors.orange),
-                        ),
+                        title: const Text("Delete Absence",
+                            style: TextStyle(color: app_colors.orange)),
                         content: DeleteConfirmationDialogContent(
                           onConfirm: () {
                             Firebasefunctions.deleteAbsenceFromSubcollection(
@@ -337,12 +319,11 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
                               widget.magmo3aModel.id,
                               widget.absenceModel.date,
                             ).catchError((error) {
-                              // Handle the error
                               print("Error deleting absence: $error");
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
                                     content:
-                                        Text('Error deleting absence: $error')),
+                                    Text('Error deleting absence: $error')),
                               );
                             });
                           },
@@ -374,7 +355,7 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
             height: 40,
           ),
         ),
-        SizedBox(height: 8), // مسافة بين الصورة والنص
+        SizedBox(height: 8),
         Text(
           label,
           style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
