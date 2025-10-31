73a6a5c608cf9efd3cd3375a556b5fb0e8b8a13a
East-Sound <kareemhekal19@gmail.com>
2025-10-31 18:44:19 +0200

    feat: Rename project and add `rename_app` package
    This commit renames the project from `fatma_elorbany_absent` to `student_management_absent` in `pubspec.yaml`.
    
    Key changes include:
    - Updating the project name.
    - Adding the `rename_app` package to `pubspec.yaml` to facilitate future app name changes.
    - Adjusting import paths across several files to use relative paths, accommodating the name change.
    - Deleting the default `widget_test.dart` file.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"fatma_elorbany_absent", origin, size)) {
+  if (!window.Create(L"Student Management Absent", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
